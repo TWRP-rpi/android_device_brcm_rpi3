7947db80dadb66909e635508722b4c8ef2079964
Giuseppe Barillari <joe2k01dev@gmail.com>
2017-12-12 15:42:55 +0100

    rpi3: Move board infos from BoardConfig to PlatformConfig
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ PlatformConfig.mk
@@ -0,0 +1,12 @@
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+
+TARGET_BOARD_PLATFORM := bcm2710
+
+# Misc
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
